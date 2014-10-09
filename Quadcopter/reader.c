d15537f5e09c3ac02763bd2b06e8ba66af38f2f0
n1k1tai <nicolas.lavelaine-de-maubeuge@ensea.fr>
2014-10-09 14:30:53 +0200

    avancement standard

--- Quadcopter/reader.c
+++ Quadcopter/reader.c
@@ -1,1 +1,19 @@
+##############################################
+#   Reader functionality                     #
+#   Nicolas de Maubeuge and Vivien Billaud   #
+#   October 2014                             #
+#                                            #
+##############################################
+
+// Library includes :
+
+
+// Hardware defines :
+
+# define RUDDER_PIN 1
+# define THROTTLE_PIN 2
+# define AILERONS_PIN 3
+# define ELEVATOR_PIN 4
+
+// Software defines :
 
