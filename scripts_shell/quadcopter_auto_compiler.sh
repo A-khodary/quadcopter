ff7d8a4929a5e6d1c0d462dca40e1f65adf7b9b0
n1k1tai <nikita@localhost.localdomain>
2014-10-12 18:33:21 -0500

    Ajout du programme de mise à jour et compilation automatique

---
+++ scripts_shell/quadcopter_auto_compiler.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+echo "[i] Program update request received"
+
+if [$1 = "-r"]; then
+	echo "[i] Relaunching program..."
+	pkill quadcopter
+	quadcopter
+fi
+
+if [$1 = "rc"]; then
+	echo "[i] Killing program..."
+	pkill quadcopter
+	echo "[i] Downloading latest source changes from github..."
+	cd ~/Quadcopter_code
+	git pull
+	rm -f  Builds/quadcopter
+	cd Quadcopter
+	echo "[i] Compiling program from sources..."
+	gcc * -o ../Builds/quadcopter
+	cd ../Builds
+	chmod +x quadcopter
+	echo "[i] Relaunching Quadcopter program..."
+	./quadcopter
+fi
+		
+
