25e7f8ec9fd434a95eb15d0bae333010e067c96c
n1k1tai <ni>
2014-10-14 22:23:19 +0200

    inclusion updates

--- Quadcopter/autopilot.h
+++ Quadcopter/autopilot.h
@@ -1,2 +1,4 @@
 #include "shared_librairies.h"
 #include "autopilot_global_variables.h"
+
+#include "imu_globals_variables.h"
--- Quadcopter/data_logger.h
+++ Quadcopter/data_logger.h
@@ -1,2 +1,6 @@
 #include "shared_librairies.h"
 #include "data_logger_global_variables.h"
+
+#include "imu_globals_variables.h"
+#include "reader_global_variables.h"
+#include "autopilot_global_variables.h"
