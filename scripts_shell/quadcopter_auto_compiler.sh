39969dad2c054d38eafc0f5799396bf1cc567110
n1k1tai <ni>
2014-10-14 22:05:33 +0200

    Minor changes

--- scripts_shell/quadcopter_auto_compiler.sh
+++ scripts_shell/quadcopter_auto_compiler.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-echo "[i] Program update request received"
 
 if [ $1 = "-r" ]; then
+	echo "[i] Program update request received"
 	echo "[i] Relaunching program..."
 	pkill quadcopter
 	quadcopter
 fi
 
 if [ $1 = "-rc" ]; then
+	echo "[i] Program update request received"
 	echo "[i] Killing program..."
 	pkill quadcopter
 	echo "[i] Downloading latest source changes from github..."
@@ -26,6 +27,7 @@ fi
 		
 
 if [ $1 = "-c" ]; then
+	echo "[i] Program update request received"
 	cd ~/Quadcopter_code
 	git pull
 	rm -f  Builds/quadcopter
@@ -34,6 +36,7 @@ if [ $1 = "-c" ]; then
 	gcc * -o ../Builds/quadcopter
 	cd ../Builds
 	chmod +x quadcopter
-	echo "[i] Relaunching Quadcopter program..."
-	./quadcopter
+	
+else then
+	echo "[e]What do you want to do ?"
 fi
