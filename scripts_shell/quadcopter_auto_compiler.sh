9afa52bfbedaee7888eeba7bcdf986ae9201c92b
n1k1tai <ni>
2014-10-14 21:59:00 +0200

    Corrections de bugs quadcopter_auto_compiler.sh et ajout d'une option

--- scripts_shell/quadcopter_auto_compiler.sh
+++ scripts_shell/quadcopter_auto_compiler.sh
@@ -2,13 +2,13 @@
 
 echo "[i] Program update request received"
 
-if [$1 = "-r"]; then
+if [ $1 = "-r" ]; then
 	echo "[i] Relaunching program..."
 	pkill quadcopter
 	quadcopter
 fi
 
-if [$1 = "-rc"]; then
+if [ $1 = "-rc" ]; then
 	echo "[i] Killing program..."
 	pkill quadcopter
 	echo "[i] Downloading latest source changes from github..."
@@ -25,3 +25,15 @@ if [$1 = "-rc"]; then
 fi
 		
 
+if [ $1 = "-c" ]; then
+	cd ~/Quadcopter_code
+	git pull
+	rm -f  Builds/quadcopter
+	cd Quadcopter
+	echo "[i] Compiling program from sources..."
+	gcc * -o ../Builds/quadcopter
+	cd ../Builds
+	chmod +x quadcopter
+	echo "[i] Relaunching Quadcopter program..."
+	./quadcopter
+fi
