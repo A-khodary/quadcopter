4a90d844c2bd4c6596474b1bc74b3595d92b336e
n1k1tai <ni>
2014-10-17 00:10:40 +0200

    Global variables headers programming

--- Quadcopter/imu_globals_variables.h
+++ Quadcopter/imu_globals_variables.h
@@ -1,1 +1,44 @@
 
+
+// Global variables structures declaration
+
+typedef struct  flightStateShared {
+        double yaw;
+        double pitch;
+        double roll;
+
+        pthread_mutex_t readWriteMutex;
+
+}flightStateShared;
+
+
+
+typedef struct rawPositionShared {
+
+    double latitude;
+    double longitude;
+    double altitude;
+
+    pthread_mutex_t readWriteMutex;
+
+}rawPositionShared;
+
+typedef struct positionShared {
+
+    double x;
+    double y;
+    double z;
+
+    pthread_mutex_t readWriteMutex;
+
+}positionShared
+
+
+
+// Global variables definitions
+
+flightStateShared quadcopterFlightStateShared;
+rawPositionShared quadcopteRawPositionShared;
+positionShared quadcopterPositionFromHomeShared;
+
+
