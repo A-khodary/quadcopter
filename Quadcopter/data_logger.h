5316ce51a01c44aee9c9743adb80efdd52d7f726
n1k1tai <nikita@localhost.localdomain>
2015-01-06 17:13:14 -0600

    Added Mavlink Protocol to project and started data logger programming

--- Quadcopter/data_logger.h
+++ Quadcopter/data_logger.h
@@ -4,7 +4,26 @@
 #include "imu_globals_variables.h"
 #include "reader_global_variables.h"
 #include "autopilot_global_variables.h"
+#include <mavlink/v1.0/common/mavlink.h>
 
-// Struc
+// Structures :
+
+
+
+// Some software defines :
+
+LOG_IMU 1
+IMU_LOGGING_PERIOD 2
+
+LOG_AUTOPILOT 1
+AUTOPILOT_LOGGING_PERIOD 1
+
+// TODO : continue
+
+// Functions prototypes
+
+void dataLoggerHandler();
+int sendData();
+int receiveData();
 
 
--- Quadcopter/data_logger_global_variables.h
+++ Quadcopter/data_logger_global_variables.h
@@ -1,3 +1,3 @@
-// CF Mavlink protocol
+
 
 
