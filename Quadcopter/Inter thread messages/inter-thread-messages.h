a0781f151afca525409dec1e7285676d37653149
vivibill <vivien.billaud@ensea.fr>
2015-01-13 16:43:23 +0100

    Added a new inter-thread messages library
    MADE BY NIKITA !!!!

---
+++ Quadcopter/Inter thread messages/inter-thread-messages.h
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+// Some defines
+
+
+
+// Structure declarations :
+
+typedef struct handler_t
+{
+
+    int handlerInitialized;
+
+    pthread_mutex_t fifoMutex;
+    message_t* fifoFirstElement;
+
+    int fifoProcessingPriority;
+    int messagesToProcess;
+
+
+}handler_t;
+
+
+typedef struct message_t
+{
+    char message[128];
+
+    int dataSize;
+    void* data;
+
+    int priority;
+    message_t* previousMessage;
+    message_t* nextMessage;
+
+}message_t;
+
+
+// Functions declarations :
+
+handler_t* initializeHandler();
+int sendMessage(handler_t* Handler, message_t* message);
+message_t* retrieveMessage(handler_t* Handler);
+
