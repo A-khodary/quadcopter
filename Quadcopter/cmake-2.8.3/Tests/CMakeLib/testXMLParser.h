51ed41560fa95b741edfc10b6db544e5d12037f3
n1k1tai <nikita@localhost.localdomain>
2015-01-27 17:14:27 -0600

    Added a Cmake list

---
+++ Quadcopter/cmake-2.8.3/Tests/CMakeLib/testXMLParser.h
@@ -0,0 +1,6 @@
+#ifndef testXMLParser_h
+#define testXMLParser_h
+
+#define SOURCE_DIR "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/Tests/CMakeLib"
+
+#endif
---
+++ Quadcopter/cmake-2.8.3/Tests/CMakeTests/CTestTestfile.cmake
@@ -0,0 +1,30 @@
+# CMake generated Testfile for 
+# Source directory: /home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/Tests/CMakeTests
+# Build directory: /home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/Tests/CMakeTests
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+ADD_TEST(CMake.List "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/bin/cmake" "-P" "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/Tests/CMakeTests/ListTest.cmake")
+ADD_TEST(CMake.VariableWatch "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/bin/cmake" "-P" "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/Tests/CMakeTests/VariableWatchTest.cmake")
+ADD_TEST(CMake.Include "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/bin/cmake" "-P" "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/Tests/CMakeTests/IncludeTest.cmake")
+ADD_TEST(CMake.FindBase "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/bin/cmake" "-P" "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/Tests/CMakeTests/FindBaseTest.cmake")
+ADD_TEST(CMake.Toolchain "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/bin/cmake" "-P" "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/Tests/CMakeTests/ToolchainTest.cmake")
+ADD_TEST(CMake.GetFilenameComponentRealpath "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/bin/cmake" "-P" "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/Tests/CMakeTests/GetFilenameComponentRealpathTest.cmake")
+ADD_TEST(CMake.Version "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/bin/cmake" "-P" "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/Tests/CMakeTests/VersionTest.cmake")
+ADD_TEST(CMake.Message "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/bin/cmake" "-P" "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/Tests/CMakeTests/MessageTest.cmake")
+ADD_TEST(CMake.File "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/bin/cmake" "-P" "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/Tests/CMakeTests/FileTest.cmake")
+ADD_TEST(CMake.ConfigureFile "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/bin/cmake" "-P" "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/Tests/CMakeTests/ConfigureFileTest.cmake")
+ADD_TEST(CMake.SeparateArguments "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/bin/cmake" "-P" "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/Tests/CMakeTests/SeparateArgumentsTest.cmake")
+ADD_TEST(CMake.ImplicitLinkInfo "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/bin/cmake" "-P" "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/Tests/CMakeTests/ImplicitLinkInfoTest.cmake")
+ADD_TEST(CMake.ModuleNotices "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/bin/cmake" "-P" "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/Tests/CMakeTests/ModuleNoticesTest.cmake")
+ADD_TEST(CMake.If "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/bin/cmake" "-P" "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/Tests/CMakeTests/IfTest.cmake")
+ADD_TEST(CMake.String "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/bin/cmake" "-P" "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/Tests/CMakeTests/StringTest.cmake")
+ADD_TEST(CMake.Math "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/bin/cmake" "-P" "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/Tests/CMakeTests/MathTest.cmake")
+ADD_TEST(CMake.CMakeMinimumRequired "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/bin/cmake" "-P" "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/Tests/CMakeTests/CMakeMinimumRequiredTest.cmake")
+ADD_TEST(CMake.CompilerIdVendor "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/bin/cmake" "-P" "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/Tests/CMakeTests/CompilerIdVendorTest.cmake")
+ADD_TEST(CMake.FileDownload "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/bin/cmake" "-P" "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/Tests/CMakeTests/FileDownloadTest.cmake")
+SET_TESTS_PROPERTIES(CMake.FileDownload PROPERTIES  PASS_REGULAR_EXPRESSION "file already exists with expected MD5 sum")
+ADD_TEST(CMake.ELF "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/bin/cmake" "-P" "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/Tests/CMakeTests/ELFTest.cmake")
+ADD_TEST(CMake.EndStuff "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/bin/cmake" "-Ddir:STRING=/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/Tests/CMakeTests/EndStuffTest" "-P" "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/Tests/CMakeTests/EndStuffTest.cmake")
+ADD_TEST(CMake.GetPrerequisites "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/bin/cmake" "-DCTEST_CONFIGURATION_TYPE:STRING=${CTEST_CONFIGURATION_TYPE}" "-P" "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/Tests/CMakeTests/GetPrerequisitesTest.cmake")
+ADD_TEST(CMake.CheckSourceTree "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/bin/cmake" "-DCMake_BINARY_DIR:PATH=/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3" "-DCMake_SOURCE_DIR:PATH=/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3" "-DCVS_EXECUTABLE:STRING=CVS_EXECUTABLE-NOTFOUND" "-DGIT_EXECUTABLE:STRING=/usr/bin/git" "-DHOME:STRING=/home/nikita" "-P" "/home/nikita/Scripting/quadcopter/Quadcopter/cmake-2.8.3/Tests/CMakeTests/CheckSourceTreeTest.cmake")
