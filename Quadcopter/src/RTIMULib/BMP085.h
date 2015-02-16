d980c55941617dd93f427d1bf2af40263524e5e1
vivibill <vivien.billaud@ensea.fr>
2015-02-16 18:49:46 +0100

    BMP085

---
+++ Quadcopter/src/RTIMULib/BMP085.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <linux/i2c-dev.h>
+#include <linux/i2c.h>
+#include <sys/ioctl.h>
+#include "smbus.h"
+
+#define BMP085_I2C_ADDRESS 0x77
+
+const unsigned char BMP085_OVERSAMPLING_SETTING = 3;
+
+int bmp085_i2c_Begin();
+__s32 bmp085_i2c_Read_Int(int fd, __u8 address); //????
+void bmp085_i2c_Write_Byte(int fd, __u8 address, __u8 value);
+void bmp085_i2c_Read_Block(int fd, __u8 address, __u8 length, __u8 *values);
+unsigned int bmp085_ReadUT();
+unsigned int bmp085_ReadUT();
+unsigned int bmp085_ReadUP();
+unsigned int bmp085_GetPressure(unsigned int up);
+unsigned int bmp085_GetTemperature(unsigned int ut);
