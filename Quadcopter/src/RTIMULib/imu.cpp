e41b63f3184f867969c348047ab02e1f0576aa72
vivibill <vivien.billaud@ensea.fr>
2015-02-19 18:21:42 +0100

    IMU integration

--- Quadcopter/src/RTIMULib/imu.cpp
+++ Quadcopter/src/RTIMULib/imu.cpp
@@ -12,15 +12,60 @@ void imuHandler(void* arg)
     currentMessage.priority = 20;
     sendMessage(mainITMHandler, currentMessage);
 
-    // TODO Init IMU
-
-    RTIMU *imu;
-    RTIMU_DATA imuData;
+    // Init IMU
 
     RTIMUSettings *settings = new RTIMUSettings("wewillfly");
     imu = RTIMU::createIMU(settings);
     imu->IMUInit();
-    imuData.fusionPose ;
+
+    // GPS init
+
+    initializeDataGPS();
+
+    // BMP085 init
+
+    //initializeBMP085();
+
+    // Send the data to the global variables
+
+    flightStateShared.roll = imuData.fusionPose.x()* RTMATH_RAD_TO_DEGREE ;
+    flightStateShared pitch= imuData.fusionPose.y()* RTMATH_RAD_TO_DEGREE;
+    flightStateShared.yaw = imuData.fusionPose.z()* RTMATH_RAD_TO_DEGREE;
+
+    // To check
+
+    double imuDataAltitude = 44330*(1-pow(pow((imuData.pressure/101325),(1/5255)),1000));
+
+    rawPositionShared.altitude = dataGPS.altitude;
+    rawPositionShared.longitude = dataGPS.longitude;
+    rawPositionShared.altitude = (dataGPS.altitude*0.5+imuDataAltitude*0.5);
+
+    /*
+
+    // Directional vector
+
+    positionShared.x = cos(flightStateShared.roll)*cos(flightStateShared.pitch);
+    positionShared.y = sin(flightStateShared.roll)*cos(flightStateShared.pitch);
+    positionShared.z = sin(flightStateShared.pitch);
+
+    */
+
+    message_t message1,message2;
+    strcpy(message1.message,"reader_imu_order_ultrasonicon");
+    strcpy(message2.message,"reader_imu_order_ultrasonicoff");
+
+    if (rawPositionShared.altitude < 4)
+    {
+        sendMessage(mainITMHandler,message1);
+        rawPositionShared.altitude = (dataGPS.altitude*0.1+imuDataAltitude*0.1+(double)receivedCommands.ultrasonicTelemeter*100*0.8);
+
+    }
+     if (rawPositionShared.altitude < 0.5)
+    {
+        sendMessage(mainITMHandler,message2);
+        rawPositionShared.altitude = (dataGPS.altitude*0.1+imuDataAltitude*0.1+(double)receivedCommands.ultrasonicTelemeter*100*0.8);
+
+    }
 
 
  }
--- Quadcopter/src/imu.h
+++ Quadcopter/src/imu.h
@@ -1,13 +1,13 @@
 #ifndef IMU
 #define IMU
 
-
 #include "shared_librairies.h"
 #include "imu_globals_variables.h"
+#include "NEO-6M.h"
+#include "BMP085.h"
+#include"reader.h"
 
 //Handler prototype :
 void imuHandler(void* arg);
-
-
 
 #endif
