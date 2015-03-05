96a3a8e46e6d881f0be00daf45a162f624c004e6
n1k1tai <nikita@localhost.localdomain>
2015-03-05 17:59:27 -0600

    added cpp

---
+++ Quadcopter/src/RTIMULib/NEO-6M.cpp
@@ -0,0 +1,54 @@
+#include "NEO-6M.h"
+
+
+
+
+int initializGPS(void)
+
+{
+
+
+    char gps[65]; //gps string array
+
+    char hours[2];
+    char mins[2];
+    char secs[2];
+    char longitude[9];
+    char latitude[10];
+    char nuSat[2];
+    char alt[5];
+
+    int fd; //handler
+    int flag;
+    int i;
+    char arr[]="$GPGGA";
+
+    //printf("raspberry gps programme");
+
+    if ((fd = serialOpen ("/dev/ttyAMA0", 9600)) < 0) //device adress, baudrate
+    {
+        fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
+        return -1;
+    }
+
+    if (wiringPiSetup () == -1)
+    {
+        fprintf (stdout, "Unable to start wiringPi: %s\n", strerror (errno)) ;
+        return -1;
+    }
+
+
+
+
+}
+
+gpsData_t getGpsData(void)
+{
+    gpsData_t gpsData;
+
+
+    return gpsData;
+}
+
+
+
