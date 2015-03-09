3ad06291fa3d1d92208767927ea26b9d3d2ca564
nikita <nikita@nikita-fuckin-desktop.home>
2015-03-09 18:08:56 +0100

    Added a ITM test project

---
+++ TestITM/testITM.c
@@ -0,0 +1,15 @@
+#include "inter-thread-messages.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+
+
+int main()
+{
+    handler_t* = initializeHandler();
+
+    return 0;
+
+}
