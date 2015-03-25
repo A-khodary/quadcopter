cc9d69de9a6121ee92511f5a5f6d7bd462c3aa65
n1k1tai <nikita@localhost.localdomain>
2015-03-25 11:54:35 -0500

    Modified imu header

--- Quadcopter/src/imu.h
+++ Quadcopter/src/imu.h
@@ -6,7 +6,6 @@
 #include "imu_globals_variables.h"
 #include "reader_global_variables.h"
 
-#include "RTIMULib/NEO-6M.h" //TODO with UBLOX parsing
 #include "RTIMULib/BMP085.h"
 #include "RTIMULib/RTIMULib.h"
 
