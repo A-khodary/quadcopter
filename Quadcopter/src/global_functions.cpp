7a7133d329c748e5ce7d512520548bf1d6eb4ef2
n1k1tai <nikita@localhost.localdomain>
2015-05-24 15:40:01 -0500

    hotfix

--- Quadcopter/src/global_functions.cpp
+++ Quadcopter/src/global_functions.cpp
@@ -39,8 +39,8 @@ int convertPlanar(double* x, double* y, double latitude, double longitude)
     int p;
     projPJ pj_merc, pj_latlong;
 
-            if (!(pj_merc = pj_init_plus("+proj=merc +ellps=clrk66 +lat_ts=49.03")) ) exit(1); // Cergy latitude
-            if (!(pj_latlong = pj_init_plus("+proj=latlong +ellps=clrk66")) ) exit(1);
+            if (!(pj_merc = pj_init_plus("+proj=merc +ellps=clrk66 +lat_ts=49.03")) ) return -1; // Cergy latitude
+            if (!(pj_latlong = pj_init_plus("+proj=latlong +ellps=clrk66")) ) return -1;
             *x *= DEG_TO_RAD;
             *y *= DEG_TO_RAD;
             p = pj_transform(pj_latlong, pj_merc, 1, 1, x, y, NULL );
