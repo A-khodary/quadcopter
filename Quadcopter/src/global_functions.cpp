4a7f02975bbc2560c743ff4b66b35115bd0c8f05
n1k1tai <nikita@localhost.localdomain>
2015-05-24 18:50:32 -0500

    hotfx

--- Quadcopter/src/global_functions.cpp
+++ Quadcopter/src/global_functions.cpp
@@ -40,13 +40,20 @@ int convertPlanarToHome(double* x, double* y, double latitude, double longitude)
 int convertPlanar(double* x, double* y, double latitude, double longitude)
 {
     int p;
+    double xcpy, ycpy = 0;
+    xcpy = *x;
+    ycpy = *y;
+
     projPJ pj_merc, pj_latlong;
 
     pj_merc = pj_init_plus("+proj=merc +ellps=clrk66 +lat_ts=33");
     pj_latlong = pj_init_plus("+proj=latlong +ellps=clrk66");
-    *x *= DEG_TO_RAD;
-    *y *= DEG_TO_RAD;
-    p = pj_transform(pj_latlong, pj_merc, 1, 1, x, y, NULL );
+    xcpy *= DEG_TO_RAD;
+    ycpy *= DEG_TO_RAD;
+    p = pj_transform(pj_latlong, pj_merc, 1, 1, &xcpy, &ycpy, NULL );
+
+    *x = xcpy;
+    *y = ycpy;
 
     return p;
 }
